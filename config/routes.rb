131330364f071160b4e87631a468c4e5367c9e89
Alexander Claes <alexander.claes10@gmail.com>
2023-07-21 17:24:58 +0200

    Disable membership routes for now

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
   resources :projects
-  resources :projects_users
+  # resources :projects_users
   resources :folders
-  resources :folders_users
+  # resources :folders_users
   resources :tasks
   resources :timesheets
 end
