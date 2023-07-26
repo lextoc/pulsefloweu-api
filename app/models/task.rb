031cb5f1e393f5f8d4b65dab416ccf9a12fb0aee
Alexander Claes <alexander.claes10@gmail.com>
2023-07-26 21:40:37 +0200

    Add relation to task

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,9 @@ class Task < ApplicationRecord
   belongs_to :user
   belongs_to :folder
 
+  has_many :task_users, dependent: :delete_all
+  has_many :users, through: :task_users
+
   validates_length_of :name, minimum: 1, maximum: 100, allow_blank: false
   validates :user, presence: true
   validates :folder, presence: true
