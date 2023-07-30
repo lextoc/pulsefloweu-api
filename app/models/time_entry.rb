eb5be2fdbcfdcb45391e09aaec718086233f1898
Alexander Claes <alexander.claes10@gmail.com>
2023-07-30 23:11:51 +0200

    Add validation for time entry

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -6,6 +6,14 @@ class TimeEntry < ApplicationRecord
   belongs_to :folder
 
   validates :start_date, presence: true
+
   validates :user, presence: true
   validates :folder, presence: true
+  validate :end_date_cannot_be_before_start_date
+
+  def end_date_cannot_be_before_start_date
+    return unless end_date.present? && end_date < start_date
+
+    errors.add(:end_date, "can't be before start date")
+  end
 end
