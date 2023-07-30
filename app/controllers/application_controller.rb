586572b86ec43abae8995dc8dec464cd39f75802
Alexander Claes <alexander.claes10@gmail.com>
2023-07-30 16:04:42 +0200

    Add avatar to permitted params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    added_attrs = %i[username first_name last_name]
+    added_attrs = %i[username first_name last_name avatar]
     devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
     devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
   end
