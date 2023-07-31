14bd573091f911be4a46d88f65bbe5441646e47a
Alexander Claes <alexander.claes10@gmail.com>
2023-07-31 22:16:05 +0200

    Add time entries by date

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -3,19 +3,31 @@ class TimeEntriesController < ApplicationController
 
   def index
     time_entries = params[:active] ? active_time_entries : all_time_entries
-    # time_entries.each { |time_entry| authorize!(:read, time_entry) }
-    # render_data(time_entries)
-    arr = []
+
+    object = {}
+
+    # Create dates with arrays.
+    time_entries.each do |time_entry|
+      object.store(time_entry.start_date.strftime('%F'), {
+                     time_entries: [],
+                     data: get_data_for_date(time_entry.start_date)
+                   })
+    end
+
+    # Store time entries in the right date array.
     time_entries.each do |time_entry|
       authorize!(:read, time_entry)
 
       extra_fields = { 'task_name' => time_entry.task.name,
-                    'folder_name' => time_entry.folder.name,
-                    'project_name' => time_entry.folder.project.name }
-      arr << JSON.parse(time_entry.to_json).merge(extra_fields)
+                       'folder_name' => time_entry.folder.name,
+                       'project_name' => time_entry.folder.project.name }
+
+      object[time_entry.start_date.strftime('%F')][:time_entries]
+        .push(JSON.parse(time_entry.to_json)
+        .merge(extra_fields))
     end
 
-    render(json: { success: true, data: arr, meta: pagination_info(time_entries) }.to_json)
+    render(json: { success: true, data: object, meta: pagination_info(time_entries) }.to_json)
   end
 
   def show
@@ -67,5 +79,20 @@ class TimeEntriesController < ApplicationController
 
   def all_time_entries
     current_user.time_entries.all.page(params[:page] || 1)
+  end
+
+  def get_data_for_date(date)
+    total_duration = 0
+    total_time_entries = 0
+
+    TimeEntry.where(start_date: date.beginning_of_day..date.end_of_day).each do |time_entry|
+      total_time_entries += 1
+      total_duration += ((time_entry.end_date ? time_entry.end_date.to_f : DateTime.now.to_f) - time_entry.start_date.to_f).to_i
+    end
+
+    {
+      total_duration:,
+      total_time_entries:
+    }
   end
 end
