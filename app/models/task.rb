d8fbab5dda4867b2089743557babdf7b45bbac2d
Alexander Claes <alexander.claes10@gmail.com>
2023-08-01 21:38:51 +0200

    Improve performance

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,19 +3,13 @@ class Task < ApplicationRecord
 
   belongs_to :user
   belongs_to :folder
-
   has_many :time_entries, dependent: :delete_all
 
-  validates_length_of :name, minimum: 1, maximum: 100, allow_blank: false
+  validates :name, presence: true, length: { minimum: 1, maximum: 100 }
   validates :user, presence: true
   validates :folder, presence: true
 
   def total_duration_of_time_entries
-    duration = 0
-    time_entries.each do |time_entry|
-      duration += ((time_entry.end_date.nil? ? Time.now.getutc.to_f : time_entry.end_date.to_f) -
-        time_entry.start_date.to_f).to_i
-    end
-    duration
+    time_entries.sum('EXTRACT(epoch FROM COALESCE(end_date, NOW()) - start_date)').to_i
   end
 end
