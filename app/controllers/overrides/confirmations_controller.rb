13a823294b2fdbc5f78815f57383b6d4a8aa27d7
Alexander Claes <alexander.claes10@gmail.com>
2023-08-15 18:18:51 +0200

    Add email confirmation new

--- app/controllers/overrides/confirmations_controller.rb
+++ app/controllers/overrides/confirmations_controller.rb
@@ -1,4 +1,29 @@
 class Overrides::ConfirmationsController < DeviseTokenAuth::ConfirmationsController
+  def new
+    @email = params[:email]
+    @user = User.find_by(email: @email)
+
+    if @user && @user.confirmed_at.nil?
+      @user.send_confirmation_instructions({
+                                             client_config: params[:config_name]
+                                           })
+      render(status: 200, json: {
+        email: @email,
+        message: 'Your request has been received. A new confirmation email has been sent.'
+      }.to_json)
+    elsif @user && @user.confirmed_at.present?
+      render(status: 200, json: {
+        email: @email,
+        message: 'This account has already been confirmed.'
+      }.to_json)
+    else
+      render(status: 404, json: {
+        email: @email,
+        message: 'No user account exists for this email.'
+      }.to_json)
+    end
+  end
+
   private
 
   def redirect_options
