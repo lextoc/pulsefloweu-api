648d4cda9a47acab7bc8a862f676a5ad926736d3
Alexander Claes <alexander.claes10@gmail.com>
2023-08-15 17:05:32 +0200

    Add necessary lines

--- app/controllers/overrides/passwords_controller.rb
+++ app/controllers/overrides/passwords_controller.rb
@@ -1,10 +1,9 @@
 class Overrides::PasswordsController < DeviseTokenAuth::PasswordsController
   private
 
-  # In case allow_other_host doesn't work, uncommenting this might fix it.
-  # def resource_params
-  #   params.permit(:email, :reset_password_token, :config, :redirect_url)
-  # end
+  def resource_params
+    params.permit(:email, :reset_password_token, :config, :redirect_url)
+  end
 
   def redirect_options
     {
